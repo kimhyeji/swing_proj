5ef98b6e6a14a8e7bc96e7edaf75694d7de54246
kimhyeji <gpwl5589@gmail.com>
2024-11-13 09:50:43 +0900

    JLabel

--- src/main/java/SwExam.java
+++ src/main/java/SwExam.java
@@ -3,15 +3,23 @@ import java.awt.*;
 
 public class SwExam extends JFrame {
     public SwExam() {
+        // Frame
         setTitle("스윙 프로젝트 연습"); // 프레임 제목 설정
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 프로그램 종료
         setSize(500, 300); // 프레임 크기 설정
         setLocationRelativeTo(null); // 화면 중앙에서 시작
 
+        // ContentPane
         Container cp = getContentPane(); // 프레임에서 컨텐트 팬 받아오기
+
+        // Layout
         cp.setLayout(null);
 //        cp.setLayout(new FlowLayout()); // 왼쪽에서 오른쪽으로 정렬
+
+        // JLabel
+        JLabel jLabel = new JLabel("Label Test");
+        jLabel.setBounds(100, 100, 100, 10);
+        cp.add(jLabel);
 
 
         setVisible(true); // 화면에 프레임 출력
